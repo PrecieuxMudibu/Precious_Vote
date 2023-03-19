41e8a55c7dcca1a34a83f241a336012b8ce3e350
PrecieuxMudibu <oceangpmb@gmail.com>
2023-03-19 14:17:19 +0100

    feat: Create the add_electors page

--- components/index.js
+++ components/index.js
@@ -3,11 +3,12 @@ import Create_Election_Card from './create_election_card';
 import Election_Card from './election_card';
 import Left_Section from './left_section';
 import Logo from './logo';
-
+import Item from './item';
 export {
     Quality_Item,
     Create_Election_Card,
     Election_Card,
     Left_Section,
     Logo,
+    Item
 };
