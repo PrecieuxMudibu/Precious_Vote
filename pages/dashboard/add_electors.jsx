c8cb50b1ae18a7a4ede4ac0d910b9cb0b286d49d
PrecieuxMudibu <oceangpmb@gmail.com>
2023-04-19 00:40:56 +0100

    feat: Add electors in the state of elector_to_create

--- pages/dashboard/add_electors.jsx
+++ pages/dashboard/add_electors.jsx
@@ -1,10 +1,83 @@
+/* eslint-disable no-prototype-builtins */
 import candidat_image from '../../public/images/test.png';
 import styles from '../../styles/dashboard/add_candidates.module.css';
 import { Item, Dashboard_Layout } from '../../components/index';
-
+import { saveAs } from 'file-saver';
+import * as XLSX from 'xlsx';
 import { Icon } from '@iconify/react';
+import Link from 'next/link';
+import { applicationContext } from '../_app';
+import { useContext, useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Add_Candidates() {
+    const { election_to_create, set_election_to_create } =
+        useContext(applicationContext);
+
+    const [file, set_file] = useState(null);
+    const [election_contains_posts, set_election_contains_posts] =
+        useState(false);
+    const [candidates, set_candidates] = useState([]);
+    const [number_of_post, set_number_of_post] = useState(0);
+
+    const { push } = useRouter();
+
+    useEffect(() => {
+        if (election_to_create.candidates[0].post != '') {
+            set_election_contains_posts(true);
+        }
+    }, [election_to_create]);
+
+    const inputFile = useRef();
+
+    async function handle_file(e) {
+        if (election_contains_posts) {
+            const file = e.target.files[0];
+            const data = await file.arrayBuffer();
+            const workbook = XLSX.read(data);
+            const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+            const jsonData = XLSX.utils.sheet_to_json(worksheet);
+            set_candidates([...jsonData]);
+
+            let electors_verified = [];
+            for (let i = 0; i < jsonData.length; i++) {
+                let current_elector = jsonData[i];
+
+                if (
+                    current_elector.hasOwnProperty('first_name') &&
+                    current_elector.hasOwnProperty('name') &&
+                    current_elector.hasOwnProperty('email')
+                ) {
+                    electors_verified.push({
+                        first_name: current_elector.first_name,
+                        name: current_elector.name,
+                        email: current_elector.email,
+                    });
+                }
+            }
+
+            if (electors_verified.length != 0) {
+                set_election_to_create({
+                    ...election_to_create,
+                    electors: electors_verified,
+                });
+            }
+        } else {
+            push(`/dashboard/my_projects`);
+        }
+    }
+
+    useEffect(() => {
+        console.log('election_to_create>>>', election_to_create);
+    }, [election_to_create]);
+    function download_template_file() {
+        fetch('/candidates_post_1.xlsx')
+            .then((res) => res.blob())
+            .then((blob) => {
+                set_file(blob);
+                saveAs(blob, 'candidates_template.xlsx');
+            });
+    }
     return (
         <Dashboard_Layout page_title="Ajoutez des électeurs">
             <section>
@@ -19,9 +92,18 @@ export default function Add_Candidates() {
                                 className={styles.upload_file_icon}
                             />
                             <p>Sélectionner un fichier Excel au format XLSX</p>
-                            <button className="button_secondary">
+                            <button
+                                onClick={() => inputFile.current.click()}
+                                className="button_secondary"
+                            >
                                 Choisir un fichier
                             </button>
+                            <input
+                                className="hidden"
+                                ref={inputFile}
+                                type="file"
+                                onInput={(e) => handle_file(e)}
+                            />
                         </div>
                         <p>Télécharger un fichier exemple en cliquant ici.</p>
                     </div>
@@ -33,50 +115,29 @@ export default function Add_Candidates() {
                             <div>Prénom</div>
                             <div>Nom</div>
                         </div>
-
-                        <Item
-                            number={1}
-                            picture={candidat_image}
-                            first_name="Belle Grace"
-                            name="Tshilanda"
-                        />
-                        <Item
-                            number={1}
-                            picture={candidat_image}
-                            first_name="Belle Grace"
-                            name="Tshilanda"
-                        />
-                        <Item
-                            number={1}
-                            picture={candidat_image}
-                            first_name="Belle Grace"
-                            name="Tshilanda"
-                        />
-                        <Item
-                            number={1}
-                            picture={candidat_image}
-                            first_name="Belle Grace"
-                            name="Tshilanda"
-                        />
-                        <Item
-                            number={1}
-                            picture={candidat_image}
-                            first_name="Belle Grace"
-                            name="Tshilanda"
-                        />
-                        <Item
-                            number={1}
-                            picture={candidat_image}
-                            first_name="Belle Grace"
-                            name="Tshilanda"
-                        />
+                        {election_to_create.electors
+                            ? election_to_create.electors.map(
+                                  (elector, index) => (
+                                      <Item
+                                          key={index}
+                                          number={index}
+                                          //   picture={elector.picture}
+                                          first_name={elector.first_name}
+                                          name={elector.name}
+                                      />
+                                  )
+                              )
+                            : null}
                     </div>
                 </div>
-                {/* <div className={styles.list_of_posts}></div> */}
 
                 <div className={styles.buttons_group}>
-                    <button className="button_primary">Précédent</button>
-                    <button className="button_primary">Suivant</button>
+                    <Link href="/dashboard/add_candidates">
+                        <button className="button_primary">Précédent</button>
+                    </Link>
+                    <Link href="/dashboard/round_parameters">
+                        <button className="button_primary">Suivant</button>
+                    </Link>
                 </div>
             </section>
         </Dashboard_Layout>
