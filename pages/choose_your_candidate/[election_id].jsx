5981f34c621ffa9ce737a576c3cf4304aed55a51
PrecieuxMudibu <oceangpmb@gmail.com>
2023-04-20 02:13:26 +0100

    update

--- pages/choose_your_candidate/[election_id].jsx
+++ pages/choose_your_candidate/[election_id].jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import styles from '../../styles/choose_your_candidate/index.module.css';
 import { Candidate_Card } from '../../components';
 import Layout from '../../components/layouts/layout';
@@ -21,12 +22,6 @@ export default function Choose_Candidate() {
         []
     );
 
-    useEffect(() => {
-        console.log('candidates Z>>>', candidates);
-        console.log('current_election Z>>>', current_election);
-        console.log('election_post_and_rounds Z>>>', election_post_and_rounds);
-    }, [candidates, current_election]);
-
     async function get_candidates_for_the_round(id) {
         return await axios
             .get(`${route_for_get_candidates_for_the_round}/${id}`)
@@ -34,29 +29,6 @@ export default function Choose_Candidate() {
             .catch((error) => {
                 console.log('error--->>>', error);
             });
-    }
-
-    async function get_candidates() {
-        const posts = await get_posts_of_election(election_id);
-
-        // GET THE ROUNDS FOR ALL POST
-        let post_and_rounds = [];
-        for (let i = 0; i < posts?.length; i++) {
-            const rounds = await get_rounds_for_a_post(posts[i]._id);
-            post_and_rounds.push({
-                post: posts[i],
-                ...rounds,
-            });
-        }
-
-        set_election_post_and_rounds(post_and_rounds);
-
-        // GET A CANDIDATES FOR THE FIRST POST
-        set_candidates(
-            await get_candidates_for_the_round(
-                post_and_rounds[post_number]?.rounds[0]._id
-            )
-        );
     }
 
     async function get_candidates() {
@@ -89,7 +61,7 @@ export default function Choose_Candidate() {
     }, [election_id, post_number]);
     return (
         <Layout>
-            <h1>{current_election.name}</h1>
+            <h1>{current_election?.name}</h1>
             <h2>Poste : {election_post_and_rounds[post_number]?.post.name}</h2>
             <div className={styles.candidates}>
                 {candidates?.map((candidate, index) => (
