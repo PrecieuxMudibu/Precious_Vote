c715d2ff13c82a98a6d79100d2346ed12bd819a8
PrecieuxMudibu <oceangpmb@gmail.com>
2023-07-10 23:17:42 +0100

    style: Delete unused comments in the choose_your_candidate page

--- pages/choose_your_candidate/[election_id].jsx
+++ pages/choose_your_candidate/[election_id].jsx
@@ -37,65 +37,6 @@ export default function Choose_Candidate() {
     console.log('election', election);
     console.log('current_post', current_post);
 
-    // const [election, set_election] = useState({});
-    // async function get_election_info() {
-    //     let response = await get_an_election(election_id);
-    //     console.log('response', response);
-    //     set_election(await get_an_election(election_id));
-    // }
-    // useEffect(() => {
-    //     get_election_info();
-    // }, [election_id]);
-
-    // // GET POST OF THE ELECTION
-    // const [posts, set_posts] = useState([]);
-    // async function get_posts() {
-    //     set_posts(await get_posts_of_election(election_id));
-    // }
-    // useEffect(() => {
-    //     get_posts();
-    // }, [election_id]);
-
-    // // GET ROUNDS OF THE POSTS
-    // const [rounds, set_rounds] = useState();
-    // async function get_rounds() {
-    //     const current_post = posts ? posts[post_index] : null;
-    //     set_rounds(await get_rounds_for_a_post(current_post?._id));
-    // }
-    // useEffect(() => {
-    //     get_rounds();
-    //     console.log('POST>>>', posts);
-    //     console.log('POSTS INDEX>>>', posts ? posts[post_index] : null);
-    // }, [posts, post_index]);
-
-    // // GET CANDIDATES OF THE ROUNDS
-    // async function get_candidates() {
-    //     // set_rounds(await get_rounds_for_a_post(current_post?._id));
-    //     console.log('rounds>>> ICICICICICI 2', rounds?.rounds[0]._id);
-    //     let candidates_rounds = await get_candidates_for_the_round(
-    //         rounds?.rounds[0]._id
-    //     );
-
-    //     const candidates_intermediates_variables = candidates_rounds?.map(
-    //         (item) => item.candidate_id
-    //     );
-
-    //     console.log(
-    //         'candidates_intermediates_variables>>>',
-    //         candidates_intermediates_variables
-    //     );
-    //     set_candidates(candidates_intermediates_variables);
-    // }
-
-    // useEffect(() => {
-    //     console.log('rounds iiiiic>>>>', rounds);
-    //     get_candidates();
-    // }, [rounds, posts, post_index]);
-
-    // useEffect(() => {
-    //     // get_rounds_for_a_post
-    //     console.log('candidates>>>', candidates);
-    // }, [candidates]);
     return (
         <Layout>
             <h1>{election?.name}</h1>
