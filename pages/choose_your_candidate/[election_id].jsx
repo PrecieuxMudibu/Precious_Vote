d6f9e6bc9c46c9b6bf07d83d229ebebff5908dfd
PrecieuxMudibu <oceangpmb@gmail.com>
2023-07-15 15:02:27 +0100

    refactor: Delete the function get_an_election and use then for get the response in the choose_your_candidate

--- pages/choose_your_candidate/[election_id].jsx
+++ pages/choose_your_candidate/[election_id].jsx
@@ -3,28 +3,30 @@ import styles from '../../styles/choose_your_candidate/index.module.css';
 import { Candidate_Card } from '../../components';
 import Layout from '../../layouts/layout';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { get_an_election } from '../../requests';
 import { Icon } from '@iconify/react';
+import { applicationContext } from '../_app';
 
 export default function Choose_Candidate() {
+    const { fake_token } = useContext(applicationContext);
+
     const { query } = useRouter();
 
     const { election_id } = query;
     const [post_index, set_post_index] = useState(0);
     const [current_post, set_current_post] = useState(0);
-
     const [election, set_election] = useState([]);
-    async function get_election_info() {
-        const token = localStorage.getItem('vote_app_token');
 
-        const data = await get_an_election(election_id, token);
-
-        set_election(data);
-    }
 
     useEffect(() => {
-        get_election_info();
+        get_an_election(election_id, fake_token)
+            .then((response) => {
+                set_election(response.data.election);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }, [query]);
 
     useEffect(() => {
