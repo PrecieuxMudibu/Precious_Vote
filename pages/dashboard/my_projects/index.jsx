350635b3002cb8cb708df314411cb2630bd8c2ea
PrecieuxMudibu <oceangpmb@gmail.com>
2023-07-23 18:29:45 +0100

    refactor: Adjust the responsive of the profile page for mobile

--- pages/dashboard/my_projects/index.jsx
+++ pages/dashboard/my_projects/index.jsx
@@ -49,16 +49,6 @@ export default function My_Projects() {
                                 <Election_Card election={election} />
                             </Link>
                         ))}
-
-                        {elections.map((election, index) => (
-                            <Link
-                                className="link"
-                                key={index}
-                                href={`/dashboard/my_projects/${election._id}`}
-                            >
-                                <Election_Card election={election} />
-                            </Link>
-                        ))}
                     </div>
                 ) : (
                     <div className="center_loader">
